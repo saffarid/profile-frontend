a6045dd11b65d81537d4e1db1d2130a7dd67be4a
PavlovNO <pavlov.nikita@mail.ru>
2022-08-31 16:30:24 +0500

    a: Оформление странички с образованием

--- vue/src/screens/education/Education.vue
+++ vue/src/screens/education/Education.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="education" ref="education">
-        <svg x="0" y="0" width="100%" height="100%">
-            <SvgEducation :x="iconSizes.x" y="33%" :width="iconSizes.width" :height="iconSizes.height"/>
 
+        <svg x="0" y="0" width="100%" height="100%">
+            <Background/>
+            <SvgEducation :x="iconSizes.x" :y="iconSizes.y" :width="iconSizes.width" :height="iconSizes.height"/>
+            <template v-for="(line, index) of lines" :key="index">
+                <Line :line="line"/>
+            </template>
         </svg>
     </div>
 </template>
@@ -14,11 +18,15 @@
       ref,
       reactive,
       watch,
-   } from 'vue'
+   }                 from 'vue'
+   import Line       from './Line'
+   import Background from './Background'
 
    export default {
       name: 'Education',
       components: {
+         Background,
+         Line,
          SvgEducation,
       },
       setup() {
@@ -26,30 +34,86 @@
          const education = ref(null)
 
          const iconSizes = reactive({
-            x:0,
-            y:0,
+            x: 0,
+            y: 0,
             height: 250,
             width: 250,
          })
 
-         watch(education, () => {
-            window.addEventListener('resize', () => {
-                calcIcon()
-            })
+         const lines = reactive([])
+
+         const calcs = () => {
             calcIcon()
+            calcLines()
+         }
+
+         watch(education, () => {
+            calcs()
+            window.addEventListener('resize', calcs)
          })
 
          const calcIcon = () => {
-            iconSizes.width = (414/1920)*window.innerWidth
-            iconSizes.height = (350/1080)*window.innerHeight
+            iconSizes.width = (414 / 1920) * window.innerWidth
+            iconSizes.height = (350 / 1080) * window.innerHeight
 
-            iconSizes.x = (250/1920)*window.innerWidth
-            iconSizes.y = (window.innerHeight - iconSizes.height - 50)/2
+            iconSizes.x = (250 / 1920) * window.innerWidth
+            iconSizes.y = (window.innerHeight - iconSizes.height - 50) / 2
+         }
+
+         const calcLines = () => {
+
+            const titles = [
+               { title: 'Среднее' },
+               { title: 'Высшее' },
+               { title: 'Дополнительное' },
+            ]
+
+            const xStart = (750 / 1920) * window.innerWidth
+            const xEnd = (1792 / 1920) * window.innerWidth
+
+            const yLineStart = (0.25*1080 / 1080) * window.innerHeight
+            const yLineEnd = (0.75*1080 / 1080) * window.innerHeight
+
+            const yLineStep = (yLineEnd - yLineStart) / (titles.length - 1)
+
+            const yStart = (0.4*1080 / 1080) * window.innerHeight
+            const yEnd = (0.6*1080 / 1080) * window.innerHeight
+
+            const yStep = (yEnd - yStart) / (titles.length - 1)
+
+            for (let i = 0; i < titles.length; i++) {
+               const f = Math.max(14, (30 / 1080) * window.innerHeight)
+
+               const yPoint = (yStart + i * yStep)
+               const yLine = (yLineStart + i * yLineStep)
+
+               console.log([yPoint, yLine, f])
+
+               const w = xEnd - xStart
+               let h = Math.abs(yPoint - yLine) + ((yPoint >= yLine)?(2*f):(0))
+
+               lines[i] = {
+                  title: titles[i].title,
+                  fontSize: f,
+                  x: xStart,
+                  y: (yPoint >= yLine) ? (yLineStart + i * yLineStep - 2*f) : (yPoint),
+                  width: w,
+                  height: h,
+                  line: [
+                     { x: 0, y: (yPoint >= yLine) ? (h) : (0) },
+                     { x: w * 0.2, y: (yPoint >= yLine) ? (2*f) : (h) },
+                     { x: w, y: (yPoint >= yLine) ? (2*f) : (h) }
+                     ],
+               }
+
+            }
+
          }
 
          return {
             education,
-            iconSizes
+            iconSizes,
+            lines,
          }
 
       },
@@ -60,6 +124,5 @@
     .education {
         height: 100%;
         width: 100%;
-        background: slateblue;
     }
 </style>
--- vue/src/screens/education/Line.vue
+++ vue/src/screens/education/Line.vue
@@ -1,6 +1,12 @@
 <template>
-    <svg :x="x" :y="y" :height="height" :width="width">
-        
+    <svg :x="line.x" :y="line.y + ((line.line[1].y >= line.line[0].y)?(strokeWidth/2):(0))" :height="line.height" :width="line.width">
+        <path :d="`M${line.line[0].x},${line.line[0].y-((line.line[1].y == line.line[0].y)?(strokeWidth/2):(0)) }
+                   L${line.line[1].x},${line.line[1].y-((line.line[1].y >= line.line[0].y)?(strokeWidth/2):(0))}
+                   ${line.line[2].x},${line.line[2].y-((line.line[1].y >= line.line[0].y)?(strokeWidth/2):(0))}`"
+              stroke="white" :stroke-width="strokeWidth" fill="transparent"/>
+        <text fill="#fff" :font-size="line.fontSize" font-family="Segoe UI" font-weight="600" text-anchor="middle">
+            <tspan :x="((line.line[2].x - line.line[1].x)/2)" :y="line.line[1].y - line.fontSize/2">{{line.title}}</tspan>
+        </text>
     </svg>
 </template>
 
@@ -10,6 +16,20 @@
    export default {
       name: 'Line',
       mixins: [SvgMixin],
+      props: {
+         line: {
+            type: Object,
+            required: true,
+         },
+      },
+      setup() {
+         const strokeWidth = 5
+
+         return {
+            strokeWidth,
+         }
+      },
+
    }
 </script>
 
