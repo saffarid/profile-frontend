8c901fdc2d2ae943527c2abde3ca922024189e82
TheMachine <pavlov.nikita@mail.ru>
2022-10-17 22:42:09 +0500

    RecyclerView: Задержка установки новой страницы на время анимации. Letters: Убран scrollbar у iFrame.

--- vue/src/components/RecyclerView.vue
+++ vue/src/components/RecyclerView.vue
@@ -1,134 +1,133 @@
 <template>
     <div class="recycler-view" @wheel="scrollOnCur($event)" :style="styleVars">
-        <component class="node-cur"  :is="screens[_screens.cur].workspace"/>
+        <component class="node-cur" :is="screens[_screens.cur].workspace"/>
         <component class="node-prev" :is="screens[_screens.prev].workspace"/>
         <component class="node-next" :is="screens[_screens.next].workspace"/>
     </div>
 </template>
 
 <script>
-   import {
-      reactive,
-      watch,
-   } from 'vue'
+    import {
+        reactive,
+        watch,
+    } from 'vue'
 
-   export default {
-      name: 'RecyclerView',
-      props: {
-         screens: {
-            type: Array,
-            required: true,
-         },
-         indexActiveScreen: {
-            type: Number,
-            required: false,
-            default: -1,
-         },
-      },
-      setup(props, context) {
+    export default {
+        name: 'RecyclerView',
+        props: {
+            screens: {
+                type: Array,
+                required: true,
+            },
+            indexActiveScreen: {
+                type: Number,
+                required: false,
+                default: -1,
+            },
+        },
+        setup(props, context) {
 
-         const _screens = reactive({})
-         const stepDelay = 20
+            const _screens = reactive({})
+            const stepDelay = 20
 
-         const z_indexes = {
-            front: '1',
-            back: '-1',
-         }
-
-         let lastScroll = new Date()
-
-         const styleVars = reactive({
-            '--z-index-prev': z_indexes.back,
-            '--z-index-next': z_indexes.front,
-            '--offset-y': 0 + 'vh',
-            '--time-animation': 0.5 + 's',
-         })
-
-         if (props.screens.length == 1){
-            _screens.prev = 0
-            _screens.cur = 0
-            _screens.next = 0
-         }
-         else if (props.screens.length == 2) {
-            _screens.prev = 1
-            _screens.cur = 0
-            _screens.next = 1
-         }
-         else if (props.screens.length > 2) {
-            _screens.prev = props.screens.length - 1
-            _screens.cur = 0
-            _screens.next = 1
-         }
-
-         const scrollOnCur = (event) => {
-            console.log(event)
-
-            if ((new Date().getTime() - lastScroll.getTime()) < parseFloat(styleVars['--time-animation'].replace('s', '')) * 100) return
-
-            styleVars['--offset-y'] = (parseInt(styleVars['--offset-y'].replace('vh', '')) - (event.deltaY) / stepDelay) + 'vh'
-
-            console.log(styleVars['--offset-y'])
-
-            if (parseInt(styleVars['--offset-y'].replace('vh', '')) < 0) {
-               if (styleVars['--z-index-next'] < styleVars['--z-index-prev']) {
-                  styleVars['--z-index-next'] = z_indexes.front
-                  styleVars['--z-index-prev'] = z_indexes.back
-               }
-
-               if (parseInt(styleVars['--offset-y'].replace('vh', '')) <= -100) {
-
-                  if (props.indexActiveScreen == -1) {
-                     setCurScreen((_screens.cur + 1 == props.screens.length) ? (0) : (_screens.cur + 1))
-                  }
-                  else {
-                     context.emit('activeScreenChange', (_screens.cur + 1 == props.screens.length) ? (0) : (_screens.cur + 1))
-                  }
-               }
+            const z_indexes = {
+                front: '1',
+                back: '-1',
             }
 
-            if (parseInt(styleVars['--offset-y'].replace('vh', '')) > 0) {
+            let lastScroll = new Date()
 
-               if (styleVars['--z-index-next'] > styleVars['--z-index-prev']) {
-                  styleVars['--z-index-next'] = z_indexes.back
-                  styleVars['--z-index-prev'] = z_indexes.front
-               }
-
-               if (parseInt(styleVars['--offset-y'].replace('vh', '')) >= 100) {
-
-                  if (props.indexActiveScreen == -1) {
-                     setCurScreen((_screens.cur - 1 == -1) ? (props.screens.length - 1) : (_screens.cur - 1))
-                  }
-                  else {
-                     context.emit('activeScreenChange', (_screens.cur - 1 == -1) ? (props.screens.length - 1) : (_screens.cur - 1))
-                  }
-
-               }
-            }
-            lastScroll = new Date()
-         }
-
-         const setCurScreen = (index) => {
-            _screens.prev = (index - 1 == -1) ? (props.screens.length - 1) : (index - 1)
-            _screens.cur = index
-            _screens.next = (index + 1 == props.screens.length) ? (0) : (index + 1)
-
-            styleVars['--offset-y'] = 0 + 'vh'
-         }
-
-         if (props.indexActiveScreen != -1) {
-            watch(props, () => {
-               setCurScreen(props.indexActiveScreen)
+            const styleVars = reactive({
+                '--z-index-prev': z_indexes.back,
+                '--z-index-next': z_indexes.front,
+                '--offset-y': 0 + 'vh',
+                '--time-animation': 0.5 + 's',
             })
-         }
 
-         return {
-            _screens,
-            scrollOnCur,
-            styleVars,
-         }
+            if (props.screens.length == 1) {
+                _screens.prev = 0
+                _screens.cur = 0
+                _screens.next = 0
+            } else if (props.screens.length == 2) {
+                _screens.prev = 1
+                _screens.cur = 0
+                _screens.next = 1
+            } else if (props.screens.length > 2) {
+                _screens.prev = props.screens.length - 1
+                _screens.cur = 0
+                _screens.next = 1
+            }
 
-      },
-   }
+            const scrollOnCur = (event) => {
+
+                if ((new Date().getTime() - lastScroll.getTime()) < parseFloat(styleVars['--time-animation'].replace('s', '')) * 100) return
+
+                styleVars['--offset-y'] = (parseInt(styleVars['--offset-y'].replace('vh', '')) - (event.deltaY) / stepDelay) + 'vh'
+
+                console.log(styleVars['--offset-y'])
+
+                if (parseInt(styleVars['--offset-y'].replace('vh', '')) < 0) {
+                    if (styleVars['--z-index-next'] < styleVars['--z-index-prev']) {
+                        styleVars['--z-index-next'] = z_indexes.front
+                        styleVars['--z-index-prev'] = z_indexes.back
+                    }
+
+                    if (parseInt(styleVars['--offset-y'].replace('vh', '')) <= -100) {
+
+                        if (props.indexActiveScreen == -1) {
+                            setCurScreen((_screens.cur + 1 == props.screens.length) ? (0) : (_screens.cur + 1))
+                        } else {
+                            context.emit('activeScreenChange', (_screens.cur + 1 == props.screens.length) ? (0) : (_screens.cur + 1))
+                        }
+                    }
+                }
+
+                if (parseInt(styleVars['--offset-y'].replace('vh', '')) > 0) {
+
+                    if (styleVars['--z-index-next'] > styleVars['--z-index-prev']) {
+                        styleVars['--z-index-next'] = z_indexes.back
+                        styleVars['--z-index-prev'] = z_indexes.front
+                    }
+
+                    if (parseInt(styleVars['--offset-y'].replace('vh', '')) >= 100) {
+
+                        if (props.indexActiveScreen == -1) {
+                            setCurScreen((_screens.cur - 1 == -1) ? (props.screens.length - 1) : (_screens.cur - 1))
+                        } else {
+                            context.emit('activeScreenChange', (_screens.cur - 1 == -1) ? (props.screens.length - 1) : (_screens.cur - 1))
+                        }
+
+                    }
+                }
+                lastScroll = new Date()
+            }
+
+            const setCurScreen = (index) => {
+                setTimeout(() => {
+                        _screens.prev = (index - 1 == -1) ? (props.screens.length - 1) : (index - 1)
+                        _screens.cur = index
+                        _screens.next = (index + 1 == props.screens.length) ? (0) : (index + 1)
+
+                        styleVars['--offset-y'] = 0 + 'vh'
+                    },
+                    parseInt(styleVars['--time-animation'].replace('s', '') * 1000)
+                )
+            }
+
+            if (props.indexActiveScreen != -1) {
+                watch(props, () => {
+                    setCurScreen(props.indexActiveScreen)
+                })
+            }
+
+            return {
+                _screens,
+                scrollOnCur,
+                styleVars,
+            }
+
+        },
+    }
 </script>
 
 <style lang="scss" scoped>
--- vue/src/screens/portfolio/cards/Letters.vue
+++ vue/src/screens/portfolio/cards/Letters.vue
@@ -2,6 +2,12 @@
     <FullCard style="--inner_color: #b43503; --outer_color: #a12f02;">
         <div class="card letters">
             <div class="title">{{data.title}}</div>
+            <div class="l">
+                <div class="link">
+                    <iframe src="./letters/elemy/letter.html" frameborder="0" scrolling="no"/>
+                </div>
+                <div class="desc"></div>
+            </div>
         </div>
     </FullCard>
 </template>
@@ -29,8 +35,39 @@
 
     .letters {
         display: grid;
-        grid-template-rows: 1fr auto;
+        grid-template-rows: 100px auto;
         row-gap: 5px;
+        align-self: stretch;
+        justify-self: stretch;
+        height: 97%;
+
+        .l {
+            display: grid;
+            grid-template-columns: 2fr 1fr;
+            align-content: stretch;
+            align-items: stretch;
+            align-self: stretch;
+            justify-content: stretch;
+            justify-items: stretch;
+            justify-self: stretch;
+
+            .link {
+                display: grid;
+                align-self: stretch;
+                justify-items: center;
+                justify-content: stretch;
+                justify-self: stretch;
+                align-content: stretch;
+                align-items: stretch;
+                overflow-y: auto;
+                iframe {
+                    width: 81.6%;
+                    height: 320%;
+                    overflow-y: hidden;
+                }
+            }
+        }
+
     }
 
 </style>
