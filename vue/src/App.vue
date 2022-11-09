26057d053b0fff51c989e72a9ef2e9e3403d961a
PavlovNO <pavlov.nikita@mail.ru>
2022-11-09 10:17:19 +0500

    Разделение App на Desktop и Mobile.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,64 +1,27 @@
 <template>
     <div class="light">
-        <HeadMenu class="top" :screens="_screens" @clickOnItem="activeScreenChange"/>
-        <RecyclerView class="middle" :screens="_screens" :index-active-screen="activeScreen"
-                      @activeScreenChange="activeScreenChange"/>
+        <AppMobile v-if="isMobile"/>
+        <AppDesktop v-else/>
     </div>
 </template>
 
 <script>
 
-   import { _screens } from './screens/screens'
-   import RecyclerView from './components/RecyclerView'
-   import HeadMenu     from './components/HeadMenu'
-   import {
-      inject,
-      provide,
-      ref,
-      reactive,
-   }                   from 'vue'
-   import {
-      asyncRequest,
-   }                   from './js/web'
+   import AppDesktop from './app/AppDesktop'
+   import AppMobile  from './app/AppMobile'
 
    export default {
       name: 'App',
       components: {
-         HeadMenu,
-         RecyclerView,
+         AppMobile,
+         AppDesktop,
       },
       setup() {
-         const api = inject('api')
-         const workObject = inject('workObject')
-         const profile = reactive({})
-
-         const activeScreen = ref(0)
-
-         provide('asyncRequest', asyncRequest)
-         provide('profile', profile)
-
-         const activeScreenChange = (index) => {
-            activeScreen.value = index
-         }
-
-         asyncRequest(api.MODEL_REQUESTS.work_e(
-            api.ESSENCE.freelance.name,
-            api.ESSENCE.freelance.actions.get,
-            ),
-            JSON.stringify({}),
-         )
-            .then(data => {
-               if (data.responseCode == api.CODES_RESPONSE.ok.responseCode) {
-                  workObject.copy(data.datas, profile)
-               }
-            })
-            .catch(err => console.error(err))
+         const isMobile = navigator.userAgent.toLowerCase().match(/mobile/i)
 
 
          return {
-            activeScreen,
-            activeScreenChange,
-            _screens,
+            isMobile,
          }
       },
    }
@@ -66,12 +29,4 @@
 
 <style lang="scss">
     @import "@/assets/style/main";
-
-    .top {
-        position: absolute;
-        z-index: 100;
-        width: 100%;
-    }
-
-
 </style>
---
+++ vue/src/app/AppMobile.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="app-mobile">
+        <span>В процессе разработки</span>
+    </div>
+</template>
+
+<script>
+   export default {
+      name: 'AppMobile',
+   }
+</script>
+
+<style lang="scss">
+    .app-mobile {
+        background-color: var(--primary_color);
+        height: 100vh;
+        display: grid;
+        justify-content: center;
+        align-content: center;
+
+        span {
+            color: white;
+        }
+    }
+</style>
